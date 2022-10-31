8f09af21f67d598e3c8ef20108e66b0bba3a62d3
Roopam2003 <agrawalroopam438@gmail.com>
2022-10-31 10:15:13 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // bringVegetables(thela: true);
+
     // double pie = 3.14;
     // bool isFemale = true;
     // num temp = 30.5;
@@ -17,7 +20,29 @@ class MyApp extends StatelessWidget {
     // const pi = 3.14;
 
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+
+      // darkTheme: ThemeData(
+      //   brightness: Brightness.dark,
+      //   primarySwatch: Colors.deepPurple,
+      // ),
+
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage(),
+      },
     );
   }
+
+// // function
+//   bringVegetables({required bool thela, int rupees = 100}) {
+//     // Take cycle
+//     // go to sector 16
+//   }
 }
